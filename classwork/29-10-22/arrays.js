55af0abe772614b0eac745c058201053aa600ca7
Umer001 <umerkhalid01@gmail.com>
2022-10-29 12:55:05 +0500

    we have learnt arrays and objects in today class.

---
+++ classwork/29-10-22/arrays.js
@@ -0,0 +1,14 @@
+const fruits = [];
+const numbers = [1, 2, 3, 4, 6, 7, 8, 9]
+fruits.push("apple");
+fruits.push("banana");
+
+fruits.unshift("Gava");//store at start of array
+fruits.shift();         //remove from start of array
+//console.log(fruits)
+
+
+let num1 = (arg) => (arg == 7);
+let num = numbers.find(num1)////return value not index
+console.log(num)
+
---
+++ classwork/29-10-22/object.js
@@ -0,0 +1,25 @@
+const obj = {
+    name: "umer", class: "BSCS", address:
+    {
+        city: "islamabad",
+        country: "Pak"
+    }
+
+};
+
+
+//console.log(obj.address.city);
+//console.log(obj["address"]["country"]);//Use when there is some variable instead of key
+
+const employer = {
+    employee_id: "24",
+    employee_name: "Talha,",
+    employee_company: "Jazz",
+    company_address:
+        { city: "Islamabad", country: "PAK" },
+    employer_address:
+        { city: "Fsd", country: "PAK" },
+    employer_salary: "50k",
+}
+
+console.log(employer.company_address.city,employer.employer_address.city);
