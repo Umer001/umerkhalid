4c6440a5ef7f66a96e04f699bbf7e089a3ab3426
Umer001 <umerkhalid01@gmail.com>
2022-10-30 10:07:20 +0500

    added string.js

---
+++ classwork/30-10-22/string.js
@@ -0,0 +1,16 @@
+let fname="Arslan"
+let lname="Ali"
+let msg="This is my city"
+
+console.log(`${fname} ${lname}`) //ES6 method for concatination
+
+console.log(msg.indexOf('city'));  //return index of work/char
+
+let city="Rawalpindi"
+
+
+console.log(city.replace("R","TY"));
+
+console.log(city.toUpperCase());
+
+console.log(msg.split(" ")[1])
