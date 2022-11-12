0ab24f9f18a6079c2cf64f7477f92e235c491f93
Umer001 <umerkhalid01@gmail.com>
2022-11-12 21:34:47 +0500

    task3.js completed

---
+++ classwork/12-11-22/task3.js
@@ -0,0 +1,11 @@
+function validate(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] + arr[j] == num && i != j) {
+        return `pair with a given sum ${num} is (${arr[i]},${arr[j]})`;
+      }
+    }
+  }
+  return "no valid pair exist for this number";
+}
+console.log(validate([1, 5, 1, 4, 8], 9));
